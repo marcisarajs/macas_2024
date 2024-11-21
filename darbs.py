1361436c85d4e798fba29c394a8a0f305e984010
ZMGnv <73342220+ZMGnv@users.noreply.github.com>
2024-11-21 08:06:53 +0200

    Izveidots darbs.py
    Praktiskais uzdevums. Dots veicamais uzdevums un risinājums. Izlabot pielaistā kļūdas!

---
+++ darbs.py
@@ -0,0 +1,7 @@
+# Uzraksti programmu, kurā  dators izvēlas 100 skaitļus robežās no 101 līdz 500. Izvēlētie skaitļi tiek izvadīti terminālī.
+
+import 
+
+for i in range():
+    random-skaitlis=random.randint(101,501,5)
+    print("random-skaitlis")
